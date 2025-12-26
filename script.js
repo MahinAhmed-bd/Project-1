7c39873304e29a7c0f14483b798132b5c22f620a
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-26 16:36:33 +0600

    Learing javaScript for projects

---
+++ script.js
@@ -0,0 +1,9 @@
+let age;
+age = Number(window.prompt("Enter your age :"));
+if (age <= 18) {
+    console.log("You cannot enter here.")
+    false;
+} else {
+    console.log("Welcome");
+    true;
+}
