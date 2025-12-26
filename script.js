e5974511b4aa93e89ca2cac3d548dd331f07584c
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-27 00:02:04 +0600

    practice some javvascript

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// let age;
-// age = Number(window.prompt("Enter your age :"));
-// if (age <= 18) {
-//     console.log("You cannot enter here.")
-//     false;
-// } else {
-//     console.log("Welcome");
-//     true;
-// }
-// document.getElementById("webhead").textContent = "hi";
+//Day 1 practices
+
+/*let age;
+age = Number(window.prompt("Enter your age :"));
+if (age <= 18) {
+    console.log("You cannot enter here.")
+    false;
+} else {
+    console.log("Welcome");
+    true;
+}
+
+document.getElementById("webhead").textContent = "hi";
+
 document.getElementById("btn1").onclick = function () {
     let text = document.getElementById("inp1").value;
     document.getElementById("webhead").innerText = text;
 }
+
+const PI = 3.1416;
+r = window.prompt("Enter radius");
+area = PI * r * r;
+console.log("The area of the circle is : " + area); */ 
