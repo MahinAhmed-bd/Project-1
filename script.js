6d6d2ce7c61617d45685e14a00c1b31bbe42ab40
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-26 23:45:15 +0600

    learning javaScript

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let age;
-age = Number(window.prompt("Enter your age :"));
-if (age <= 18) {
-    console.log("You cannot enter here.")
-    false;
-} else {
-    console.log("Welcome");
-    true;
+// let age;
+// age = Number(window.prompt("Enter your age :"));
+// if (age <= 18) {
+//     console.log("You cannot enter here.")
+//     false;
+// } else {
+//     console.log("Welcome");
+//     true;
+// }
+// document.getElementById("webhead").textContent = "hi";
+document.getElementById("btn1").onclick = function () {
+    let text = document.getElementById("inp1").value;
+    document.getElementById("webhead").innerText = text;
 }
